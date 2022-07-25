2809b9e6f36ca446934ed2394e462ae738a5a439
MadarasiD <madarka0913@gmail.com>
2022-07-25 16:38:07 +0200

    create route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React from 'react'
 import './App.css';
 import "materialize-css/dist/css/materialize.min.css";
-import "materialize-css/dist/js/materialize.min.js"
+import "materialize-css/dist/js/materialize.min.js";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Experiences from './components/Experiences';
+import Educations from './components/Educations';
+import Portfolios from './components/Portfolios';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Welcome to Resume</h1>
-    </div>
+    <Router>
+        <>
+          <Route exact path="/" component={Home}/>
+            <Routes>
+              <Route exact path="/skills" component={Skills}/>
+              <Route exact path="/experiences" component={Experiences}/>
+              <Route exact path="/educations" component={Educations}/>
+              <Route exact path="/portfolios" component={Portfolios}/>
+            </Routes>
+        </>
+    </Router>
   );
 }
 
