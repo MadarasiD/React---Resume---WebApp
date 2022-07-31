3d1f403011036c65fdc02c8803cccc7be14c2732
MadarasiD <madarka0913@gmail.com>
2022-07-31 12:09:26 +0200

    update navbar

--- src/components/Navbarr.js
+++ src/components/Navbarr.js
@@ -1,30 +1,47 @@
 import React from "react";
 import { HashLink as Link } from "react-router-hash-link";
 import "materialize-css/dist/css/materialize.min.css";
+import { TbHome } from "react-icons/tb";
+import { GiSkills } from 'react-icons/gi'
+
+const mystyle = {
+    display: "flex",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center"
+  };
 
 const Navbarr = () => {
   return (
     <>
       <nav>
-        <div bg='grey' class="nav-wrapper teal darken-1">
-          <a href="/" class="brand-logo">
-            Madarasi Dániel
-          </a>
+        <div class="nav-wrapper teal darken-1">
+                <Link to='/' class="brand-logo">
+                    <i style={{paddingLeft: '15px'}}> Madarasi Dániel</i>
+              </Link>
+          
           <a href="#" data-target="mobile-demo" class="sidenav-trigger">
             <i class="material-icons">menu</i>
           </a>
           <ul class="right hide-on-med-and-down">
             <li>
-              <a href="/home">Home</a>
+              <Link to='/'>
+                    <i style={mystyle}> <TbHome size="medium"  style={{paddingRight: '5px'}}/> Home</i>
+              </Link>
             </li>
             <li>
-              <a href="badges.html">Components</a>
+            <Link to='/skills'>
+                    <i style={mystyle}> <GiSkills  /> Skills</i>
+              </Link>
             </li>
             <li>
-              <a href="collapsible.html">Javascript</a>
+              <a href="/experiences">Experiences</a>
             </li>
             <li>
-              <a href="mobile.html">Mobile</a>
+              <a href="/educations">Educations</a>
+            </li>
+            <li>
+              <a href="/portfolios">Portfolios</a>
             </li>
           </ul>
         </div>
