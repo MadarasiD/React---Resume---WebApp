c9965d0195e8ce0ffed3b8fc97d298ec50c873d4
MadarasiD <madarka0913@gmail.com>
2022-07-31 20:52:53 +0200

    update about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,59 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <h4 className="mt-bootm">
+            <strong>ABOUT ME</strong>
+            </h4>
+            <p className="grey-text">
+            2015-ben kezdtem pályafutásomat Beültetőgép-kezelő munkakörben, amit később SMT Termelési
+              Csoportvezető pozíció váltott fel .
+              Sok tanulás és fejlődés árán Integrált SMT Termelési Csoportvezetői pozícióba léptem át ( gépes hibákat
+              technikusi szinten elhárítása ), ahonnan sikerült még előrébb jutni a Senior SMT Termelési Csoportvezető
+              munkakörbe ( műszakvezető helyettes - ez kb. 1év ) ahonnan visszalépve Integrált SMT Termelési
+              Csoportvezetői pozícióba jelenleg is folytatom munkásságomat.
+              Az itt töltött évek során elvégeztem munka mellett az érettségit és most az Informatika felé elindulva Junior
+              Frontend Fejlesztő képzésen veszek részt.
+              <ul>
+                <li>
+                   <h5><u>Eddig tanult Frontend Skillek:</u></h5>  
+                </li>
+
+                <ul>
+                  <li>HTML5</li>
+                  <li>CSS3</li> 
+                  <li>BOOTSTRAP</li>
+                  <li>SASS/SCSS</li>
+                  <li>JAVASCRIPT</li>
+                  <li>GIT</li>
+                  <li>NODE.JS</li>
+                  <li>REACT</li>
+                  <li>SZOFTVERTESZTELÉS</li>
+                  <li>ANGULAR -> Folyamatban van </li>
+                  <li>HOSTING - FIREBASE ( Már csak ez van hátra ) </li>
+                </ul>
+              
+              </ul>
+              
+              Szeretnék a továbbiakban a Frontend irányában munkát találni.
+          </p>
+        </div>
+
+        <div className="card-action">
+           <h6>Personal Info</h6>
+           <div className="row mt">
+            <div className="col x16 16 m6 s12">
+                  <p>
+                    <strong>Address:</strong>
+                  </p>
+            </div>
+
+           </div>
+        </div>
+      </div>
+      </div>
   )
 }
 
