05f6fbf0a1c5937a48a9cc264d863182b8bdb674
MadarasiD <madarka0913@gmail.com>
2022-08-02 19:56:39 +0200

    Education update

--- src/components/Educations.js
+++ src/components/Educations.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import { HashLink as Link } from "react-router-hash-link";
+
 
 const Educations = () => {
   return (
-    <div>Educations</div>
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <h6>
+            <strong><u>Tanulmányaim:</u></strong>
+          </h6>
+          <table className="striped">
+            <thead>
+              <tr>
+                <th>Bizonyítvány:</th>
+                <th>Dátum:</th>
+                <th></th>
+              </tr>
+              <thead>
+                <tr>
+                  <td>Érettségi bizonyítvány</td>
+                  <td>2019.06.21</td>
+                  <td>
+                    <Link to='/' className='btn cyan lighten-1'>
+                      Itt megtekintheted:
+                    </Link>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Junior Frontend Fejlesztő</td>
+                  <td>2022.08...?</td>
+                  <td>
+                    <Link to='/' className='btn cyan lighten-1'>
+                      Itt megtekintheted:
+                    </Link>
+                  </td>
+                </tr>
+              </thead>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div>
+        <div className="card">
+            <div className="card-content">
+                <h6>
+                    <u>Szakmai készségek:</u>
+                </h6>
+                <div className="row mt-top">
+                    <div className="col s6">
+                        <p>HTML</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
