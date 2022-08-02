d38692dc58b7a7b597a2f04fd94a9d658591fec4
MadarasiD <madarka0913@gmail.com>
2022-08-02 21:28:34 +0200

    skills update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,11 +7,11 @@ const Skills = () => {
         
       <div className="card">
         <div className="card-content">
-          <h6>
+          <h5>
             <u>Szakmai készségek:</u>
-          </h6>
+          </h5>
           <div className="row mt-top">
-            <div className="col s6">
+            <div className="col s4">
                 <p>HTML - HyperText Markup Language</p>
                 <div className="progress grey lighten-1">
                     <div 
@@ -23,7 +23,7 @@ const Skills = () => {
                 </div>
             </div>
 
-            <div className="col s6">
+            <div className="col s4">
                 <p>Css - Cascading Style Sheets</p>
                 <div className="progress grey lighten-1">
                     <div 
@@ -34,6 +34,160 @@ const Skills = () => {
 
                 </div>
               </div>
+
+              <div className="col s4">
+                <p>Bootstrap</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '60%'}} >
+                        
+                    </div>
+
+                </div>
+            </div>
+              
+            </div>
+
+            <div className="row mt-top">
+            
+
+            <div className="col s4">
+                <p>Tailwindcss</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '50%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+
+              <div className="col s4">
+                <p>Materializecss</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '30%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+
+              <div className="col s4">
+                <p>JS - Javascript</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '70%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+              
+            </div>
+
+            
+            <div className="row mt-top">
+            
+
+            <div className="col s4">
+                <p>Git - Gitbash</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '70%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+
+              <div className="col s4">
+                <p>React</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '80%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+
+              <div className="col s4">
+                <p>Angular</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '40%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+              
+            </div>
+
+            <div className="row mt-top">
+            
+
+            <div className="col s4">
+                <p>Node.js</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '20%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+
+              <div className="col s4">
+                <p>Hosting - Firebase</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '0%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+
+              <div className="col s4">
+                <p>Sass / SCSS</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '40%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+              
+            </div>
+            <div className="row mt-top">
+            
+
+            <div className="col s4">
+                <p>Szoftvertesztelés</p>
+                <div className="progress grey lighten-1">
+                    <div 
+                    className="determinate blue"
+                    style={{ width: '15%'}} >
+                        
+                    </div>
+
+                </div>
+              </div>
+
+            
               
             </div>
           </div>
