d02c0ac8378230a21d3a76e3990e611fc3b57211
MadarasiD <madarka0913@gmail.com>
2022-08-02 20:33:47 +0200

    scrollindicator end

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbarr from './Navbarr'
 import Profile from './Profile'
 import About from './About'
@@ -9,6 +9,22 @@ import Portfolios from './Portfolios'
 import "../styles/ScrollIndicator.css"
 
 const Home = () => {
+    const [ scrollTop, setScrollTop] = useState(0);
+
+    const onScroll = () => {
+        const winScroll = document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+        const scrolled = ( winScroll / height) * 100;
+
+        setScrollTop(scrolled);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll)
+    }, []);
   return (
             <section>
                 
@@ -16,7 +32,7 @@ const Home = () => {
                     
                 <Navbarr />
                 <div className='progressMainWrapper'>
-                    <div className="progressMainStyle">
+                    <div className="progressMainStyle" style={{width: `${scrollTop}%`}}>
                     </div>
                     </div>
                     <div className="container">
