3add9ef8692978e1a56938d402e5f74b8a7ba5bc
MadarasiD <madarka0913@gmail.com>
2022-08-02 19:44:56 +0200

    about section end

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,16 @@ const About = () => {
       <div className="card">
         <div className="card-content">
           <h4 className="mt-bootm">
-            <strong>ABOUT ME</strong>
+            <strong><u>RÓLAM:</u></strong>
             </h4>
-            <p className="grey-text">
-            2015-ben kezdtem pályafutásomat Beültetőgép-kezelő munkakörben, amit később SMT Termelési
+            <p className="grey-darken-3-text">
+              2015-ben kezdtem pályafutásomat Beültetőgép-kezelő munkakörben, amit később SMT Termelési
               Csoportvezető pozíció váltott fel .
               Sok tanulás és fejlődés árán Integrált SMT Termelési Csoportvezetői pozícióba léptem át ( gépes hibákat
               technikusi szinten elhárítása ), ahonnan sikerült még előrébb jutni a Senior SMT Termelési Csoportvezető
               munkakörbe ( műszakvezető helyettes - ez kb. 1év ) ahonnan visszalépve Integrált SMT Termelési
               Csoportvezetői pozícióba jelenleg is folytatom munkásságomat.
-              Az itt töltött évek során elvégeztem munka mellett az érettségit és most az Informatika felé elindulva Junior
+              Az itt töltött évek során elvégeztem munka mellett az érettségit és most az Informatika irányában elindulva Junior
               Frontend Fejlesztő képzésen veszek részt.
               <ul>
                 <li>
@@ -32,7 +32,7 @@ const About = () => {
                   <li>NODE.JS</li>
                   <li>REACT</li>
                   <li>SZOFTVERTESZTELÉS</li>
-                  <li>ANGULAR -> Folyamatban van </li>
+                  <li>ANGULAR</li>
                   <li>HOSTING - FIREBASE ( Már csak ez van hátra ) </li>
                 </ul>
               
@@ -43,11 +43,30 @@ const About = () => {
         </div>
 
         <div className="card-action">
-           <h6>Personal Info</h6>
+           <h6><u>Személyes adatok:</u></h6>
            <div className="row mt">
             <div className="col x16 16 m6 s12">
                   <p>
-                    <strong>Address:</strong>
+                    <strong>Address:</strong> 3580 Tiszaújváros
+                  </p>
+
+                  <p>
+                    <strong>Email:</strong> madarka0913@gmail.com
+                  </p>
+
+                  <p>
+                    <strong>Phone:</strong> +36 30 645 64 14
+                  </p>
+            </div>
+
+            <div className="col x16 16 m6 s12">
+                
+                  <p>
+                    <strong>Anyanyelv:</strong> Magyar
+                  </p>
+
+                  <p>
+                    <strong>Második nyelv:</strong> Angol
                   </p>
             </div>
 
